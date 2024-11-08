02eaaf9290c5ee58bd9eaccf830d8f6f587b55f4
alex-astudillo <105954347+ast-alex@users.noreply.github.com>
2024-11-08 19:43:30 -0300

    arrancando inquilino

---
+++ app/src/main/java/com/laboratorio/appinombiliariaast/ui/inquilino/InquilinoViewModel.java
@@ -0,0 +1,7 @@
+package com.laboratorio.appinombiliariaast.ui.inquilino;
+
+import androidx.lifecycle.ViewModel;
+
+public class InquilinoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
